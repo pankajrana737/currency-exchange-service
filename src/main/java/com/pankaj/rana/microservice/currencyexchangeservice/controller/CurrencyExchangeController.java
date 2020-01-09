fe56a50068fbcff2ca1185713c2fc58388688015
pankajrana737 <pankajrana737@gmail.com>
2020-01-09 15:48:02 +0530

    dfdfdf

--- src/main/java/com/pankaj/rana/microservice/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/pankaj/rana/microservice/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,6 +3,8 @@ package com.pankaj.rana.microservice.currencyexchangeservice.controller;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,7 @@ import com.pankaj.rana.microservice.currencyexchangeservice.repository.ExchangeV
 
 @RestController
 public class CurrencyExchangeController {
-	
+	 private Logger logger= LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private Environment  environment;
 	@Autowired
@@ -22,6 +24,7 @@ public class CurrencyExchangeController {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue reteriveExchangeValue(@PathVariable String from, @ PathVariable String to) {
+		logger.info("inside  "+ "getCurrencyConversionbeanFeign");
 		ExchangeValue exchangeValue = exchangeRepository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		return exchangeValue;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into exchange_value(id,currency_from,currency_to,convertion_multiple,port) values(101,'USD','INR',65,8000);
 insert into exchange_value(id,currency_from,currency_to,convertion_multiple,port) values(102,'EUR','INR',78,8001);
 insert into exchange_value(id,currency_from,currency_to,convertion_multiple,port) values(103,'AUD','INR',25,8002);
+
+insert into exchange_value(id,currency_from,currency_to,convertion_multiple,port) values(104,'YEN','INR',80,8002);
